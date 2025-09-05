39c8f81e9563aaa3f819ff8edbc836800fe5974d
kacharesama3-source <kacharesama3@gmail.com>
2025-09-05 11:11:11 +0530

    java my first file

---
+++ Casting.java
@@ -0,0 +1,18 @@
+package Datatypes;
+
+public class Casting {
+
+	public static void main(String[] args) {
+	// double d=12.34;//8 bytes ,64bits
+	 //System.out.println(d);
+	 //int i= (int)d;//4 bytes ,32bits
+	 //System.out.println(i);
+	 //norrowing ,explicit,data loss 
+	 int age=30;
+	 System.out.println(age);
+	 double d =age;
+	 System.out.println(d);
+	 //winding,implict,no data loss
+	}
+
+}
