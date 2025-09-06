b71ccd06fa11ea31b9ed97f5a1a31290e0e1e353
kacharesama3-source <kacharesama3@gmail.com>
2025-09-06 12:09:35 +0530

    combination primitive data

---
+++ specialcase.java
@@ -0,0 +1,30 @@
+package Datatypes;
+
+public class specialcase {
+		public static void main(String[] args) {
+			int age=21;
+			float salary=40000.34f;
+			double bankbalance = 5454.76;
+			char sam='@';
+			long address= 1234567336;
+			String str="samssdgshjgffhfch";
+	     System.out.println(age);
+	     System.out.println(salary);
+	     System.out.println(bankbalance);
+	     System.out.println(sam);
+	     System.out.println(address);
+	     System.out.println(str);
+	     System.out.println(age+salary);
+	     System.out.println(sam+age);
+	     System.out.println(str+salary);
+	     
+	     //int store only whole number(age,cars,house)
+	     //float store decimal number (salary,bankblance,whight)
+	     //double store same as a float but large value
+	     //char store only one value at a time in single quote
+	     //long store larger value perform mathematical operation  
+		
+
+	}
+
+}
