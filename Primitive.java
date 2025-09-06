7df7c56f1ba2d001007ca106353aa8a201985836
kacharesama3-source <kacharesama3@gmail.com>
2025-09-06 11:46:03 +0530

    primitive data type

---
+++ Primitive.java
@@ -0,0 +1,19 @@
+package Datatypes;
+   public class Primitive {
+
+			public static void main(String[] args) {
+				int age =45; // store only whole number 
+				System.out.println(age);
+				double salary =4000.56; // same as flaot but long size
+				System.out.println(salary);
+				float num = 234.56f; // end with small f;
+				System.out.println(num);
+				char A = '@'; // only one character 
+				System.out.println(A);
+				long bankbalance = 1232345667;// upto 10 digit 
+				short data = 12111;
+				System.out.println(data);
+				
+	}
+
+}
