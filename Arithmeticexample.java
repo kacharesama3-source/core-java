ad3d7c98989274d323bff80584168738c007c4b3
kacharesama3-source <kacharesama3@gmail.com>
2025-09-06 13:19:31 +0530

    Arithmetic opertion

---
+++ Arithmeticexample.java
@@ -0,0 +1,15 @@
+package Datatypes;
+
+public class Arithmeticexample {
+
+	public static void main(String[] args) {
+		int a = 20;
+		int b = 56;
+		System.out.println("a+b = " + (a+b));
+		System.out.println("a-b = " + (a-b)); // +  symbol  for string concatenation
+        System.out.println("a*b =" + (a*b));
+        System.out.println("a/b ="+(a/b));
+        
+	}
+
+}
