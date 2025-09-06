0fa9627093ae63e15f0d41f5320fc5b9870b6900
kacharesama3-source <kacharesama3@gmail.com>
2025-09-06 13:34:12 +0530

    logical operation &&,|| code

---
+++ Logicaloperations.java
@@ -0,0 +1,19 @@
+package Datatypes;
+
+public class Logicaloperations {
+
+	public static void main(String[] args) {
+		int x =345; int y =678;
+		System.out.println(x>200 && y>500); // in logical   & both condition true then ture otherwise flase 
+		System.out.println(x>500 && y>200);
+		// logical or || atleast one condition true  
+		 System.out.println(x>200|| y>900);
+	
+		 System.out.println(x>600||y>900);
+		 // logical ! , 
+		 System.out.println(!(x>400)); 
+		
+	}
+	
+
+}
