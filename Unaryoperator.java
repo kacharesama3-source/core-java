33791e0b227a952dbe9c4ce3bccb4b52cb656178
kacharesama3-source <kacharesama3@gmail.com>
2025-09-08 20:04:10 +0530

    operators

---
+++ Unaryoperator.java
@@ -0,0 +1,19 @@
+package Datatypes;
+
+public class Unaryoperator {
+	public static void main(String[]args) {
+		int a=10;
+		int b=20;
+		boolean flag = true;
+		System.out.println(+a);// 10
+		System.out.println(-20);//-10
+		System.out.println(++a);//11(pre-increment)
+		System.out.println(--b);//19(pre-decrement)
+		System.out.println(!flag);//flase
+		System.out.println(~5);//-6
+		
+		
+		
+	}
+
+}
