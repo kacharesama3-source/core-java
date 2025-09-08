39027c43bd77be6e35bb33465e0c7d90e68f265c
kacharesama3-source <kacharesama3@gmail.com>
2025-09-08 12:40:49 +0530

    simple and quicker without changing meaning

---
+++ Shorthand.java
@@ -0,0 +1,22 @@
+package Datatypes;
+
+public class Shorthand {
+
+	public static void main(String[] args) {
+		//it is simple and quick for using without changing meaning ,its arithmetic operation 
+		int num =26;
+		num+=10;// num=num+10 
+		num+=20;
+		
+		System.out.println(num-2);//56-2=54
+		
+		System.out.println(num);//56
+		System.out.println(num*4);//56*4
+		System.out.println(56*4);
+	
+		
+		
+		
+		}
+
+}
