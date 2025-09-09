14ed34e9f2d5bd159601ba32bd7106ba0576d4f6
kacharesama3-source <kacharesama3@gmail.com>
2025-09-09 20:31:37 +0530

    all opeations

---
+++ Revision_relational.java
@@ -0,0 +1,16 @@
+package Datatypes;
+
+public class Revision_relational {
+
+	public static void main(String[] args) {
+		int a =45;
+		int b =30;
+		System.out.println(a>50);
+		System.out.println(b<=30);
+		System.out.println(a !=b);
+		System.out.println(a==b);
+		
+
+	}
+
+}
