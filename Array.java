06f4b89d041335816740b2034465aaea29450deb
kacharesama3-source <kacharesama3@gmail.com>
2025-09-10 11:56:35 +0530

    arrays operations

---
+++ Array.java
@@ -0,0 +1,19 @@
+package Datatypes;
+
+public class Array {
+     int[] arr = {10,20,30,40,40,50,60,70};
+     for (i=0;i<arr.length; i++)
+    	 System.out.println(arr[i])
+    	
+		
+
+		
+
+	
+	
+		        
+		    }
+		
+	
+
+}
