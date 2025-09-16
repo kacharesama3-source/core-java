380c72d69bd11a93430f91525226f68896eab871
kacharesama3-source <kacharesama3@gmail.com>
2025-09-16 12:43:16 +0530

    table of 45

---
+++ loops/Table.java
@@ -0,0 +1,14 @@
+package Datatypes.loops;
+
+public class Table {
+
+	public static void main(String[] args) {
+		int num=45;
+		for( int mul=1;mul<=10; mul++) {
+			System.out.println(num*mul);
+			
+		} 
+		System.out.println("code executes");
+	}
+
+}
