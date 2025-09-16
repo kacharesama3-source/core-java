e2b1bbfbe5650bfd016be8f619be432db402927b
kacharesama3-source <kacharesama3@gmail.com>
2025-09-16 11:53:31 +0530

    NEW FOLDER

---
+++ loops/For_loop.java
@@ -0,0 +1,26 @@
+package Loops;
+
+public class For_loop {
+
+	public static void main(String[] args) {
+	 // doing one thing again and again till the specific block execution 
+		// in for loop => initialization, condition, operations
+		// ICBO>= initialization, Condition,  Block of execution, operations(unary => increment & decrement) 
+		
+		 for ( int i=0; i<=10; i++) {
+			System.out.println(i);
+		 }
+	 
+	for ( double d = 34.9; d>20.7; d--) {
+		 System.out.println(d);
+		 
+	}  int sum =0;
+	for (int i =0; i<10; i++) {
+		sum+=i;
+			}
+	System.out.println(+sum);
+	
+
+
+}
+}
