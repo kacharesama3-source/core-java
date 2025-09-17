2adb1dfe4c98d4331478750f29007176ee37a4a8
kacharesama3-source <kacharesama3@gmail.com>
2025-09-17 13:09:45 +0530

    while loop operation

---
+++ loops/whileloop.java
@@ -0,0 +1,21 @@
+package Datatypes.loops;
+
+public class whileloop {
+   // number of iteration unknown or depends on a condition at runtime 
+	 //in while intilization before loop 
+	public static void main(String[] args) {
+		int num=1; // intilization , 
+	while ( num<100) {// condition
+		System.out.println(num); //block  
+		num++; // operation 
+		// sal 
+		int sal= 1;
+		while(sal<19999) {
+			System.out.println(sal);
+			sal++;
+		}
+	}
+
+	}
+
+}
